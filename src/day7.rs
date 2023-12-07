3be3fd35151caa26ac0f53b956656c8abe24f202
Lilac <144847922+lilac@users.noreply.github.com>
2023-12-07 08:39:52 -0500

    feat(day7): scaffolding

---
+++ src/day7.rs
@@ -0,0 +1,45 @@
+use crate::DaySolution;
+
+pub struct Day7;
+
+impl DaySolution for Day7 {
+    fn part1(input: &str) -> String {
+        "".to_string()
+    }
+    fn part2(input: &str) -> String {
+        "".to_string()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(
+            Day7::part1(
+                "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"
+            ),
+            "".to_string()
+        );
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(
+            Day7::part2(
+                "32T3K 765
+T55J5 684
+KK677 28
+KTJJT 220
+QQQJA 483"
+            ),
+            "".to_string()
+        );
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod day3;
 mod day4;
 mod day5;
 mod day6;
+mod day7;
 
 use day1::Day1;
 use day2::Day2;

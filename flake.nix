2a82cd9aa361f6cb9da1054fcc794ea7e6fd1fb1
Lilac <144847922+semitruck@users.noreply.github.com>
2023-12-08 21:47:53 -0500

    feat(nix): remove shell.nix, use flake-parts with devshell

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:numtide/devshell";
+  };
+
+  outputs = inputs @ {
+    flake-parts,
+    nixpkgs,
+    ...
+  }:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      imports = [
+        inputs.devshell.flakeModule
+      ];
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        devshells.default = with pkgs; {
+          commands = [
+            {
+              package = cargo;
+              category = "Rust";
+            }
+            {
+              package = rust-analyzer;
+              category = "Rust";
+            }
+            {
+              package = rustfmt;
+              category = "Rust";
+            }
+          ];
+        };
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+      };
+    };
+}
--- shell.nix
+++
@@ -1,6 +0,0 @@
-{pkgs ? import <nixpkgs> {}}:
-with pkgs;
-  mkShell {
-    nativeBuildInputs = [rustc cargo];
-    buildInputs = [rust-analyzer rustfmt];
-  }

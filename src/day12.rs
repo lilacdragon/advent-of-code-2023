fb333afa51d1ac18beb676f56e8c28debdbc2ac9
Lilac <144847922+semitruck@users.noreply.github.com>
2023-12-12 14:45:33 -0500

    feat(day12): star two naive solution
    Unsurprisingly, this never finishes.

--- src/day12.rs
+++ src/day12.rs
@@ -1,3 +1,6 @@
+use std::iter;
+
+use itertools::Itertools;
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 
 use crate::DaySolution;
@@ -15,7 +18,33 @@ impl DaySolution for Day12 {
             .to_string()
     }
     fn star_two(input: &str) -> String {
-        "".to_string()
+        let tiles: Vec<Row> = input.lines().map(Row::from).collect();
+
+        tiles
+            .into_par_iter()
+            .map(|row| {
+                let mut new_tiles = Vec::with_capacity(row.tiles.len() * 5 + 5);
+                for _ in 0..5 {
+                    new_tiles.extend_from_slice(&row.tiles);
+                    new_tiles.push(Tile::Unknown);
+                }
+                new_tiles.pop();
+                let new_target_groups = row.target_groups.repeat(5);
+                let unknown_idxs = new_tiles
+                    .iter()
+                    .enumerate()
+                    .filter(|(_, t)| **t == Tile::Unknown)
+                    .map(|(i, _)| i)
+                    .collect();
+                Row {
+                    tiles: new_tiles,
+                    target_groups: new_target_groups,
+                    unknown_idxs,
+                }
+            })
+            .map(|row| row.possible_arrangements())
+            .sum::<usize>()
+            .to_string()
     }
 }
 
@@ -26,6 +55,7 @@ enum Tile {
     Unknown,
 }
 
+#[derive(Debug)]
 struct Row {
     tiles: Vec<Tile>,
     target_groups: Vec<usize>,
@@ -83,6 +113,7 @@ impl Row {
         }
         groups
     }
+
     fn possible_arrangements(&self) -> usize {
         fn recurse(
             tiles: &[Tile],
@@ -90,6 +121,12 @@ impl Row {
             unknown_idxs: &[usize],
             target_groups: &[usize],
         ) -> usize {
+            let mut filled_tiles = tiles.to_vec();
+            for (idx, tile) in unknown_idxs.iter().zip(unknown_fills.iter()) {
+                filled_tiles[*idx] = *tile;
+            }
+            let groups = Row::groups(&filled_tiles);
+
             if unknown_fills.len() != unknown_idxs.len() {
                 let mut next_is_operational = unknown_fills.clone();
                 next_is_operational.push(Tile::Operational);
@@ -99,15 +136,11 @@ impl Row {
                     + recurse(tiles, next_is_damaged, unknown_idxs, target_groups);
             }
 
-            let mut tiles = tiles.to_vec();
-            for (idx, tile) in unknown_idxs.iter().zip(unknown_fills.iter()) {
-                tiles[*idx] = *tile;
-            }
-            let groups = Row::groups(&tiles);
             if groups == *target_groups {
-                return 1;
+                1
+            } else {
+                0
             }
-            0
         }
 
         recurse(
@@ -140,6 +173,16 @@ mod tests {
 
     #[test]
     fn test_star_two() {
-        assert_eq!(Day12::star_two(""), "".to_string());
+        assert_eq!(
+            Day12::star_two(
+                "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"
+            ),
+            "525152".to_string()
+        );
     }
 }
